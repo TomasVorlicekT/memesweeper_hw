576e84fce4a9b444384afb5898ea35991b5ff117
BananaJoe <tomas-vorlicek@seznam.cz>
2025-02-09 13:17:42 +0100

    Base: MemeField and Tile classes
    Functional display of the tiles, no input yet

---
+++ Engine/MemeField.cpp
@@ -0,0 +1,130 @@
+#include "MemeField.h"
+
+void MemeField::Tile::SpawnMeme()
+{
+	assert(!hasMeme);
+	hasMeme = true;
+}
+
+bool MemeField::Tile::HasMeme() const
+{
+	return hasMeme;
+}
+
+void MemeField::Tile::Reveal()
+{
+	assert(state == State::Hidden);
+	state = State::Revealed;
+}
+
+bool MemeField::Tile::IsRevealed() const
+{
+	return state == State::Revealed;
+}
+
+void MemeField::Tile::ToggleFlag()
+{
+	assert(state == State::Hidden || state == State::Flagged);
+
+	if (state == State::Hidden)
+	{
+		state = State::Flagged;
+	}
+	else if (state == State::Flagged)
+	{
+		state = State::Hidden;
+	}
+}
+
+bool MemeField::Tile::IsFlagged() const
+{
+	return state == State::Flagged;
+}
+
+void MemeField::Tile::Draw(Graphics& gfx, const Vei2& screenPos) const
+{
+	switch (state)
+	{
+	case State::Hidden:
+		SpriteCodex::DrawTileButton(screenPos, gfx);
+		break;
+
+	case State::Flagged:
+		SpriteCodex::DrawTileButton(screenPos, gfx);
+		SpriteCodex::DrawTileFlag(screenPos, gfx);
+		break;
+
+	case State::Revealed:
+		if (hasMeme)
+		{
+			SpriteCodex::DrawTileBomb(screenPos, gfx);
+		}
+		else
+		{
+			SpriteCodex::DrawTile0(screenPos, gfx);
+		}
+		break;
+
+	}
+}
+
+MemeField::MemeField(int nMemes)
+{
+	assert(nMemes > 0 && nMemes < width * height);
+
+	std::random_device rd{};
+	std::mt19937 rng{ rd() };
+	std::uniform_int_distribution<int> xDist(0, width - 1);
+	std::uniform_int_distribution<int> yDist(0, height - 1);
+
+	for (int nSpawned = 0; nSpawned < nMemes; nSpawned++)
+	{
+		Vei2 spawnPos = { xDist(rng), yDist(rng) };
+		do
+		{
+			spawnPos.x = xDist(rng);
+			spawnPos.y = yDist(rng);
+		} while (TileAt(spawnPos).HasMeme());
+
+		TileAt(spawnPos).SpawnMeme();
+	}
+
+	for (int reveal = 0; reveal < 50; reveal++)
+	{
+		Vei2 revealLoc = { xDist(rng), yDist(rng) };
+		if (!TileAt(revealLoc).IsRevealed())
+		{
+			TileAt(revealLoc).Reveal();
+		};
+	}
+
+}
+
+void MemeField::Draw(Graphics& gfx) const
+{
+	gfx.DrawRect(GetRect(), SpriteCodex::baseColor);
+
+	for (Vei2 startPos = { 0, 0 }; startPos.y < height; startPos.y++)
+	{
+		for (startPos.x = 0; startPos.x < width; startPos.x++)
+		{
+			Vei2 gridPositionCurrent = { startPos.x, startPos.y };
+			TileAt(gridPositionCurrent).Draw(gfx, gridPositionCurrent * SpriteCodex::tileSize);
+		}
+	}
+}
+
+RectI MemeField::GetRect() const
+{
+	return RectI(0, width * SpriteCodex::tileSize, 0, height * SpriteCodex::tileSize);
+}
+
+MemeField::Tile& MemeField::TileAt(const Vei2 & gridPos)
+{
+	return field[gridPos.y * width + gridPos.x];
+}
+
+const MemeField::Tile& MemeField::TileAt(const Vei2& gridPos) const
+{
+	return field[gridPos.y * width + gridPos.x];
+}
---
+++ Engine/MemeField.h
@@ -0,0 +1,53 @@
+#ifndef MEMEFIELD_H
+#define MEMEFIELD_H
+
+#include <assert.h>
+#include <random>
+#include "Graphics.h"
+#include "SpriteCodex.h"
+#include "Vei2.h"
+
+class MemeField 
+{
+private:
+	class Tile
+	{
+	public:
+		enum class State
+		{
+			Hidden,
+			Revealed,
+			Flagged
+		};
+
+	public:
+		void SpawnMeme();
+		bool HasMeme() const;
+		void Reveal();
+		bool IsRevealed() const;
+		void ToggleFlag();
+		bool IsFlagged() const;
+		void Draw(Graphics& gfx, const Vei2& screenPos) const;
+
+	private:
+		bool hasMeme{ false };
+		State state{ State::Hidden };
+	};
+
+public:
+	MemeField(int nMemes);
+	void Draw(Graphics& gfx) const;
+	RectI GetRect() const;
+
+private:
+	Tile& TileAt(const Vei2& gridPos);
+	const Tile& TileAt(const Vei2& gridPos) const;
+
+private:
+	static constexpr int width = 16;
+	static constexpr int height = 20;
+	Tile field[width * height];
+
+};
+
+#endif
